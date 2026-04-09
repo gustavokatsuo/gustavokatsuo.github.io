b24b12fbe5916ec763d0b89de047d1f1fa362d88
Gustavo <katsuo.gustavo@usp.br>
2026-04-09 11:54:20 -0300

    Teste

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,9 @@ export default function Home() {
     <main style={{ padding: '50px', fontFamily: 'sans-serif' }}>
       <h1>Olá, mundo!</h1>
       <p>Este é o meu novo portfólio feito com Next.js.</p>
+      <p>Página Pessoal e para a disciplina MAC0102.</p>
+      <p>Meu nome é Gustavo Katsuo! Prazer em conhecer!</p>
+      <p>Se você está vendo essa página, saiba que ela não vai ficar assim.</p>
     </main>
   );
 }
